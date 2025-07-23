014cd8a6230180ca5793c5f058f8dde4596647b5
PRATEEK-Fixbug <134445643+PRATEEK-Fixbug@users.noreply.github.com>
2025-07-23 23:38:44 +0530

    Add files via upload

---
+++ backend/models/Reservation.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const reservationSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  date: { type: Date, required: true },
+  time: { type: String, required: true },
+  guests: { type: Number, required: true }
+}, { timestamps: true });
+
+const Reservation = mongoose.model("Reservation", reservationSchema);
+
+module.exports = Reservation;
